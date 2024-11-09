783608dd68e6aaedbd14749364c06a2ab52b0721
shahar9840 <92571880+shahar9840@users.noreply.github.com>
2024-11-09 16:07:18 +0200

    7 commit

--- Server.py
+++ Server.py
@@ -17,12 +17,12 @@ from controllers.songs import GetSongs
 app = Flask(__name__)
 
 # connect the server to database
-CORS(app, resources={r"/*": {"origins": "https://moveo-task-front.vercel.app/"}})
+CORS(app, resources={r"/*": {"origins": "https://moveo-task-front.vercel.app"}})
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:OfszSEcPnycLQiTGJWxLVgFwloynFJmf@autorack.proxy.rlwy.net:24126/railway'
 db.init_app(app)
 app.config['JWT_SECRET_KEY'] = 'kmfksdfkv;l3mkf4l4fl3'
 app.config['SECRET_KEY']='kmfksdfkv;l3mkf4l4fl3'
-socketio.init_app(app,  resources={r"/*": {"origins": "https://moveo-task-front.vercel.app/"}}) 
+socketio.init_app(app,  resources={r"/*": {"origins": "https://moveo-task-front.vercel.app"}}) 
 
 #connect the server to api manager
 api = Api(app)
@@ -45,4 +45,4 @@ api.add_resource(GetSongs,'/get_songs')
 api.add_resource(isSinger,'/is_singer')
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True,cors_allowed_origins="https://your-frontend-domain.com" , host='0.0.0.0', allow_unsafe_werkzeug=True)
+    socketio.run(app, debug=True,cors_allowed_origins="https://moveo-task-front.vercel.app" , host='0.0.0.0', allow_unsafe_werkzeug=True)
