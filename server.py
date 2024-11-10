a9926c06904d8d9243493e4c124a24518de26ba2
shahar9840 <92571880+shahar9840@users.noreply.github.com>
2024-11-10 10:36:01 +0200

    34

--- server.py
+++ server.py
@@ -47,4 +47,4 @@ api.add_resource(GetSongs,'/get_songs')
 api.add_resource(isSinger,'/is_singer')
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app, debug=True,cors_allowed_origins="*")
